e2e115e745108656962c5c02225194d240e75a76
Rabie Z. Abou-Taleb <rabie@aboutaleb.de>
2021-11-16 11:24:29 +0100

    Update zfs.py
    spare == AVAIL <==> vdev == ONLINE

--- zfs.py
+++ zfs.py
@@ -110,7 +110,7 @@ def vdev_list(errors):
         'free': int(x[3]) if x[3].isdigit() else 0,
         'frag': int(x[6]) if x[6].isdigit() else 0,
         'usage': int(x[7]) if x[7].isdigit() else 0,
-        'online': int(x[9] == 'ONLINE'),
+        'online': int(x[9] == 'ONLINE' or x[9] == 'AVAIL'),
         'errors': errors[x[0]] if isinstance(errors, dict) and x[0] in errors else {"read": 0,"write": 0,"cksum": 0},
     } for x in r if x[0].startswith('/')}
 
